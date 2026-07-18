a4c7af6eb474abcfd5fdf5223902b5e299b6c4cf
Manikandan M <manimmaths@gmail.com>
2026-07-18 16:36:47 +0530

    Create ReverseDoubleLinkedListTraversal.java

---
+++ test16072026/ReverseDoubleLinkedListTraversal.java
@@ -0,0 +1,35 @@
+
+
+
+/*
+
+Problem Statement:-
+Vinu manages an inventory system for a small warehouse. Each item in the inventory has a unique identification number. Vinu needs a program that helps him keep track of the items by appending each new item to a list and then printing the list of items in reverse order. This will help him verify the most recently added items first.
+You are tasked with writing a program that creates a doubly linked list to manage this inventory. The program should allow Vinu to append new items to the end of the list and then print the items in reverse order of their addition.
+
+Input format :-
+The first line contains an integer N, representing the number of items to be added.
+The second line contains an integer representing the ID of an item, separated by a space.
+
+Output format :-
+The output displays the list of item IDs in reverse order, separated by spaces.
+
+Refer to the sample output for the formatting specifications.
+Code constraints :-
+1 ≤ N ≤ 10
+1 ≤ ID ≤ 1000
+
+Sample test cases :-
+Input 1 :-
+5
+104 90 167 2 98
+Output 1 :-
+98 2 167 90 104 
+
+Input 2 :-
+10
+371 18 288 342 196 357 224 295 68 35
+Output 2 :-
+35 68 295 224 357 196 342 288 18 371 
+
+*/
