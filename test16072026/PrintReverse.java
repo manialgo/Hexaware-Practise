c33847a181422c96171bdabee25b651f9ddab713
Manikandan M <manimmaths@gmail.com>
2026-07-18 10:18:57 +0530

    Create PrintReverse.java

---
+++ test16072026/PrintReverse.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class PrintReverse{
+
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+        int[] nums = new int[size];
+        for(int i=0; i<size; i++) nums[i] = sc.nextInt();
+        Arrays.sort(nums);
+        for(int i=size-1; i>=0; i--)  System.out.print((i==0) ? nums[i] : nums[i]+" ");
+        sc.close();
+    }
+}
+
+/*
+
+Problem Statement:-
+Raju, an aspiring programmer, is working on a sorting program using the merge sort algorithm. He wants to sort the array and print the sorted array in descending order. Write a program to implement the merge sort algorithm.
+
+Input format :-
+The first line consists of an integer N, representing the number of elements in the array.
+The second line consists of N space-separated integers, representing the elements of the array.
+
+Output format :-
+The output displays N space-separated integers sorted in descending order.
+
+Refer to the sample output for formatting specifications.
+
+Code constraints :-
+1 ≤ N ≤ 20
+1 ≤ elements of the array ≤ 100
+
+Sample test cases :-
+Input 1 :-
+8
+78 34 23 76 12 10 4 79
+Output 1 :-
+79 78 76 34 23 12 10 4 
+
+Input 2 :-
+5
+6 5 4 3 2
+Output 2 :-
+6 5 4 3 2 
+
+*/
