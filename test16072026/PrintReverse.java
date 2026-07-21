0bfd0e16e794cea0dec6834132c86b5aaaab40ac
Manikandan M <manimmaths@gmail.com>
2026-07-21 10:11:17 +0530

    Update PrintReverse.java

--- test16072026/PrintReverse.java
+++ test16072026/PrintReverse.java
@@ -1,3 +1,70 @@
+//TEST
+import java.util.Scanner;
+
+public class PrintReverse{
+	
+	private static void mergeSort(int[] nums, int left, int right) {
+		if(left < right) {
+			int mid = left + (right-left)/2;
+			
+			mergeSort(nums, left, mid);
+			mergeSort(nums, mid+1, right);
+			
+			merge(nums, left, mid, right);
+		}
+	}
+	
+	private static void merge(int[] nums, int left, int mid, int right) {
+		int n1 = mid-left+1;
+		int n2 = right-mid;
+		
+		int[] L = new int[n1];
+		int[] R = new int[n2];
+		
+		for(int i=0; i<n1; i++) L[i] = nums[left+i];
+		for(int j=0; j<n2; j++)	R[j] = nums[mid+1+j];
+		
+		int i=0, j=0, k=left;
+		
+		while(i<n1 && j<n2) {
+			if(L[i] < R[j]) {
+				nums[k] = L[i];
+				i++;
+			}else {
+				nums[k] = R[j];
+				j++;
+			}
+			k++;
+		}
+		
+		while(i<n1) {
+			nums[k] = L[i];
+			k++;
+			i++;
+		}
+		
+		while(j<n2) {
+			nums[k] = R[j];
+			k++;
+			j++;
+		}
+	}
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int size = sc.nextInt();
+		int[] nums = new int[size];
+		
+		for(int i=0; i<size; i++) nums[i] = sc.nextInt();
+		
+		mergeSort(nums, 0, size-1);
+		
+		for(int i=0; i<size; i++) System.out.print(nums[i]+" ");
+		sc.close();
+	}
+}
+
+/*
 import java.util.Scanner;
 import java.util.Arrays;
 
@@ -14,6 +81,7 @@ public class PrintReverse{
         sc.close();
     }
 }
+*/
 
 /*
 
