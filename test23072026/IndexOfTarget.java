07413055eb79362f3265853713c085000979d7eb
Manikandan M <manimmaths@gmail.com>
2026-07-26 15:13:51 +0530

    Update IndexOfTarget.java

--- test23072026/IndexOfTarget.java
+++ test23072026/IndexOfTarget.java
@@ -34,8 +34,6 @@ Before being passed to your function, nums is possibly rotated at an unknown piv
 
 Given the array nums after the possible rotation and an integer target, return the index of target if it is in nums, or -1 if it is not in nums.
 
-You must write an algorithm with O(log n) runtime complexity.
-
 Example 1:-
 Input:-
 nums = 4 5 6 7 0 1 2, target = 0
