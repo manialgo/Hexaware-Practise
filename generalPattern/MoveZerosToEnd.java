3752ba5980ca531405fef77cb3e38a3b4666e0a5
Manikandan M <manimmaths@gmail.com>
2026-07-31 10:03:38 +0530

    Create MoveZerosToEnd.java

---
+++ generalPattern/MoveZerosToEnd.java
@@ -0,0 +1,75 @@
+import java.util.Scanner;
+
+public class MoveZerosToEnd {
+	/* METHOD 1
+	private static void moveZeros(int[] nums) {
+		int idx=0, size = nums.length;
+		for(int i=0; i<size; i++) {
+			if(nums[i] != 0)	nums[idx++] = nums[i];
+		}
+		for(int i=idx; i<size; i++) nums[i]=0;
+	}
+	*/
+	
+	/* METHOD 2
+	private static void leftShift(int[] nums, int left, int right) {
+		int temp = nums[left];
+		for(int i=left; i<right; i++) {
+			nums[i] = nums[i+1];
+		}
+		nums[right] = temp;
+	}
+	
+	private static void moveZeros(int[] nums, int noZeros) {
+		int size = nums.length, count=0;
+		for(int i=0; i<size; i++) {
+			if(nums[i] == 0 && count <= noZeros) {
+				leftShift(nums, i, size-1);
+				i--;
+				count++;
+			}
+			if(count > noZeros) break;
+		}
+	}
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int size = sc.nextInt(), noZeros=0;
+		int[] nums = new int[size];
+		for(int i=0; i<size; i++) {
+			if((nums[i] = sc.nextInt()) == 0) noZeros++;
+		}
+		moveZeros(nums, noZeros);
+		for(int i=0; i<size; i++) System.out.print(nums[i]+" ");
+		sc.close();
+	}
+	
+	*/
+	
+	private static void moveZeros(int[] nums) {
+		int i, nonZeroPos=0, temp;
+		for(i=0; i<nums.length; i++) {
+			if(nums[i] != 0) {
+				temp = nums[i];
+				nums[i] = nums[nonZeroPos];
+				nums[nonZeroPos++] = temp;
+			}
+		}
+	}
+  
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int size = sc.nextInt();
+		int[] nums = new int[size];
+		for(int i=0; i<size; i++) {
+			nums[i] = sc.nextInt();
+		}
+		moveZeros(nums);
+		for(int i=0; i<size; i++) {
+      System.out.print(nums[i]);
+      if(i<size-1) System.out.print(" ");
+    }
+		sc.close();
+	}
+  
+}
